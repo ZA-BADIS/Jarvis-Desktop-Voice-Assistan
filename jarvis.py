24f0e3ce2eacc0136c3c44cfeb51be93d719945a
ZA-BADIS <zammalib@gmail.com>
2023-01-10 18:23:01 +0100

    Jarvis-Desktop-Voice-Assistant-main

---
+++ jarvis.py
@@ -0,0 +1,256 @@
+import pyttsx3
+import requests
+import datetime
+import speech_recognition as sr
+import wikipedia
+import webbrowser as wb
+import os
+import random
+import pyautogui
+
+
+
+
+
+
+engine = pyttsx3.init()
+
+
+
+
+
+
+def get_weather_report(city):
+    res = requests.get(
+        f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPENWEATHER_APP_ID}&units=metric").json()
+    weather = res["weather"][0]["main"]
+    temperature = res["main"]["temp"]
+    feels_like = res["main"]["feels_like"]
+    return weather, f"{temperature}℃", f"{feels_like}℃"
+
+
+
+
+def find_my_ip():
+    ip_address = requests.get('https://api64.ipify.org?format=json').json()
+    return ip_address["ip"]
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def time():
+    Time = datetime.datetime.now().strftime("%I:%M:%S")
+    speak("the current time is")
+    speak(Time)
+    print("The current time is ", Time)
+
+
+
+
+def date():
+    day = int(datetime.datetime.now().day)
+    month = int(datetime.datetime.now().month)
+    year = int(datetime.datetime.now().year)
+    speak("the current date is")
+    speak(day)
+    speak(month)
+    speak(year)
+    print("The current date is " + str(day) + "/" + str(month) + "/" + str(year))
+
+def wishme():
+    print("Welcome back sir badiss!!")
+    speak("Welcome back sir badiss i'm so happy to meet you !!")
+    
+    hour = datetime.datetime.now().hour
+    if hour >= 4 and hour < 12:
+        speak("Good Morning Sir!!")
+        print("Good Morning Sir!!")
+    elif hour >= 12 and hour < 16:
+        speak("Good Afternoon Sir!!")
+        print("Good Afternoon Sir!!")
+    elif hour >= 16 and hour < 24:
+        speak("Good Evening Sir!!")
+        print("Good Evening Sir!!")
+    else:
+        speak("Good Night Sir, See You Tommorrow")
+
+    speak("Jarvis at your service sir, please tell me how may I help you.")
+    print("Jarvis at your service sir, please tell me how may I help you.")
+
+def screenshot():
+    img = pyautogui.screenshot()
+    img.save("ss.jpg")
+
+
+
+
+def takecommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language="en-in")
+        print(query)
+
+    except Exception as e:
+        print(e)
+        speak("Please say that again")
+        return "Try Again"
+
+    return query
+
+if __name__ == "__main__":
+    wishme()
+    while True:
+        query = takecommand().lower()
+        if "time" in query:
+            time()
+
+        elif "date" in query:
+            date()
+
+        elif "who are you" in query:
+            speak("I'm JARVIS created by Mr. Badisss and I'm a desktop voice assistant.")
+            print("I'm JARVIS created by Mr. Badiss and I'm a desktop voice assistant.")
+
+        elif "how are you" in query:
+            speak("I'm fine sir, What about you?")
+            print("I'm fine sir, What about you?")
+
+        elif "fine" in query:
+            speak("Glad to hear that sir!!")
+            print("Glad to hear that sir!!")
+
+        elif "good" in query:
+            speak("Glad to hear that sir!!")
+            print("Glad to hear that sir!!")
+
+        elif "wikipedia" in query:
+            try:
+                speak("Ok wait sir, I'm searching...")
+                query = query.replace("wikipedia","")
+                result = wikipedia.summary(query, sentences=2)
+                print(result)
+                speak(result)
+            except:
+                speak("Can't find this page sir, please ask something else")
+        
+        elif "open youtube" in query:
+            wb.open("youtube.com")
+        
+        elif 'ip address' in query:
+            ip_address = find_my_ip()
+            speak(f'Your IP Address is {ip_address}.\n For your convenience, I am printing it on the screen sir.')
+            print(f'Your IP Address is {ip_address}')
+
+        elif 'weather' in query:
+            ip_address = find_my_ip()
+            city = requests.get(f"https://ipapi.co/{ip_address}/city/").text
+            speak(f"Getting weather report for your city {city}")
+            weather, temperature, feels_like = get_weather_report(city)
+            speak(f"The current temperature is {temperature}, but it feels like {feels_like}")
+            speak(f"Also, the weather report talks about {weather}")
+            speak("For your convenience, I am printing it on the screen sir.")
+            print(f"Description: {weather}\nTemperature: {temperature}\nFeels like: {feels_like}")
+   
+
+
+
+
+        elif "open google" in query:
+            wb.open("google.com") 
+   
+        elif "open stackoverflow" in query:
+            wb.open("stackoverflow.com")
+        
+        elif "play music" in query:
+            song_dir = "C:\\Users\\zamma\\Music\\Playlists"
+            songs = os.listdir(song_dir)
+            print(songs)
+            x = len(songs)
+            y = random.randint(0,x)
+            os.startfile(os.path.join(song_dir, songs[y]))
+
+        elif "open folder" in query: 
+            folderPath = "C:\\Users\\zamma\\OneDrive\\Bureau\\Nouveau dossier (2)"
+            os.startfile(folderPath)
+
+        elif "open chrome" in query:
+            chromePath = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+            os.startfile(chromePath)
+
+        elif "search on chrome" in query:
+            try:
+                speak("What should I search?")
+                print("What should I search?")
+                chromePath = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+                search = takecommand()
+                wb.get(chromePath).open_new_tab(search)
+                print(search)
+
+            except Exception as e:
+                speak("Can't open now, please try again later.")
+                print("Can't open now, please try again later.")
+
+
+        elif "search" in query:
+
+            query= query.replace("jarvis","")
+            query=query.replace("search","")
+            speak("searching...")
+            speak(query)
+            speak ("in google")
+            wb.open ("https://www.google.com/search?q="+query)
+            
+        
+        elif "you remember that" in query:
+            speak("What should I remember")
+            data = takecommand()
+            speak("You said me to remember that" + data)
+            print("You said me to remember that " + str(data))
+            remember = open("data.txt", "w")
+            remember.write(data)
+            remember.close()
+
+        elif "do you remember anything" in query:
+            remember = open("data.txt", "r")
+            speak("You told me to remember that" + remember.read())
+            print("You told me to remember that " + str(remember))
+
+        elif "screenshot" in query:
+            screenshot()
+            speak("I've taken screenshot, please check it")
+
+        elif "open command promt" in query:
+            os.system("start cmd")
+
+        elif "open code" in query:
+            codePath = "C:\\Users\\zamma\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+
+
+
+       
+        elif "click my photo" in query:
+            pyautogui.press("super")
+            pyautogui.typewrite("camera")
+            pyautogui.press("enter")
+            pyautogui. sleep(2)
+            speak("SMILE")
+            pyautogui.press("enter")
+
+
+        elif "offline" in query:
+            
+            print ("take offline")
+            speak("goodbye sir badiss i will miss you")
+            quit.alert()
+            pyautogui.alert('This is the message to display.')
+
+
